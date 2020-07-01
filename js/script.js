2503daa56f34c4b87f3b344a63a174a849bfeec9
fahd <fahadkhan8341822644@gmail.com>
2020-07-01 22:44:16 +0530

    added typewriter on home page

--- js/script.js
+++ js/script.js
@@ -22,3 +22,22 @@ window.onscroll = function() {progress()}
     document.querySelector('.pageUp').style.display="none"
   }
  }
+ let texts=['A Creative Freelancer','A Full Stack Developer', 'A UI/UX Designer'];
+ let count=0;
+ let index=0;
+ let currentText='';
+ let letter=''
+ function type(){
+  if(count===texts.length){
+    count=0
+  }
+  currentText=texts[count]
+  letter=currentText.slice(0,index++)
+  document.querySelector('.typewriter').textContent=letter
+  if(letter.length===currentText.length){
+    count++;
+    index=0;
+  }
+  setTimeout(type,400)
+ }
+ type()
