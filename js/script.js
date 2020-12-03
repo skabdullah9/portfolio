a38e8c33a380efd7856efececec1d3759df6c50d
fahd8341 <fahadkhan8341822644@gmail.com>
2020-12-03 13:44:24 +0530

    navbar is smart now

--- js/script.js
+++ js/script.js
@@ -1,55 +1,88 @@
+const navlinks = document.querySelectorAll('.nav-link');
 let sendMsgBtn = document.querySelector('.send-msg-btn');
-sendMsgBtn.addEventListener('click',()=>alert('Coming Soon...'));
+sendMsgBtn.addEventListener('click', () => alert('Coming Soon...'));
 $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
-window.onscroll = function() {progress()}
- function progress(){
+});
+window.onscroll = function () { progress() }
+function progress() {
   let windowScroll = document.body.scrollTop || document.documentElement.scrollTop
-  let height= document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (windowScroll / height) * 100;
+  document.getElementById('progress-bar').style.width = scrolled + "%";
 
-  let scrolled = (windowScroll/height)*100;
-  document.getElementById('progress-bar').style.width=scrolled+"%";
-  
-  
-  if (windowScroll>=800){
-    document.querySelector('.pageUp').style.display="inline-block"
+
+  if (windowScroll >= 800) {
+    document.querySelector('.pageUp').style.display = "inline-block"
   } else {
-    document.querySelector('.pageUp').style.display="none"
+    document.querySelector('.pageUp').style.display = "none"
   }
- }
- let texts=['A Creative Freelancer','A Front-End Developer', 'A UI/UX Designer'];
- let count=0;
- let index=0;
- let currentText='';
- let letter=''
- function type(){
-  if(count===texts.length){
-    count=0
+
+  if(windowScroll >= 0 && windowScroll < 500){
+    activeNavlink(navlinks[0])
   }
-  currentText=texts[count]
-  letter=currentText.slice(0,index++)
-  document.querySelector('.typewriter').textContent=letter
-  if(letter.length===currentText.length){
+  else if (windowScroll>=500 && windowScroll < 1300) {
+    activeNavlink(navlinks[1])
+  } 
+  else if(windowScroll>=1300 && windowScroll < 2300) {
+    activeNavlink(navlinks[2])
+  }
+  else {
+    activeNavlink(navlinks[4])
+  }
+
+}
+let texts = ['A Creative Freelancer', 'A Front-End Developer', 'A UI/UX Designer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = ''
+function type() {
+  if (count === texts.length) {
+    count = 0
+  }
+  currentText = texts[count]
+  letter = currentText.slice(0, index++)
+  document.querySelector('.typewriter').textContent = letter
+  if (letter.length === currentText.length) {
     count++;
-    index=0;
+    index = 0;
   }
-  setTimeout(type,400)
- }
- type();
+  setTimeout(type, 400)
+}
+type();
 
- $(function(){
-   let navbar = $("#navbarNav");
-   navbar.on('click', 'a', null, function() {
-     navbar.collapse('hide');
-   });
- });
+$(function () {
+  let navbar = $("#navbarNav");
+  navbar.on('click', 'a', null, function () {
+    navbar.collapse('hide');
+  });
+});
 
- $(function() {
-   $(document).click(function (event) {
-     $('#navbarNav').collapse('hide');
-   })
- })
+$(function () {
+  $(document).click(function (event) {
+    $('#navbarNav').collapse('hide');
+  })
+})
+
+
+navlinks.forEach(navlink => {
+  navlink.addEventListener('click', () => {
+   activeNavlink(navlink)
+  })
+})
+
+function activeNavlink(navlink){
+  navlinks.forEach(navlink => {
+    navlink.classList.remove('clicked');
+  });
+  if (navlink.classList.contains('contact-btn')) {
+    navlink.childNodes[1].classList.add('btn-clicked')
+  } else {
+    navlink.classList.add('clicked')
+    navlinks[4].childNodes[1].classList.remove('btn-clicked')
+  }
+}
