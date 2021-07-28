cc13fc4101b01850faaf9dcff3f5e13cfe44f59c
skabdullah9 <skabdullah9941@gmail.com>
2021-07-28 21:38:45 +0530

    todo app added

--- projects/script.js
+++ projects/script.js
@@ -1,4 +1,4 @@
-let folders= ['1_Drum_kit', 'https://covid19-live-report.netlify.app/', '3_FlexBox_Gallery', '4_Mouse_Move_Shadow', 'https://weather-report-vue.netlify.app/','6_Text_To_Speech','7_Stripe_Follow_Along_Nav', '8_Bookmarks_App', 'https://netinfo.netlify.app/','10_Hotel_Menu_Site', '11_Number_Facts', '12_piano_app', '13_Pick_Color_Game', '14_String_to_Array', '15_tabs', '16_Typing_Game', 'https://firechatonline.netlify.app/','18_Quote_Generator', '19_randomHex', '20 _reviews' ]
+let folders= ['1_Drum_kit', 'https://covid19-live-report.netlify.app/', '3_FlexBox_Gallery', '4_Mouse_Move_Shadow', 'https://weather-report-vue.netlify.app/','6_Text_To_Speech','https://todontodo.netlify.app/', '8_Bookmarks_App', 'https://netinfo.netlify.app/','10_Hotel_Menu_Site', '11_Number_Facts', '12_piano_app', '13_Pick_Color_Game', '14_String_to_Array', '15_tabs', '16_Typing_Game', 'https://firechatonline.netlify.app/','18_Quote_Generator', '19_randomHex', '20 _reviews' ]
 
 document.addEventListener('DOMContentLoaded',()=>{
 const thumbnails = document.querySelectorAll('.grid__item')
