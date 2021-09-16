232f0823d036cfedbcb5cf7ef0a757b2b1c7d6e9
skabdullah9 <skabdullah9941@gmail.com>
2021-09-16 19:31:41 +0530

    responsive bugs

--- projects/8_Pick_Color_Game/script.js
+++ projects/8_Pick_Color_Game/script.js
@@ -1,111 +1,121 @@
-const boxes = document.querySelectorAll('.box');
-const dragableBoxContainer = document.querySelector('.dragable-box-container')
-const scoreContainer = document.querySelector('.score')
-const title = document.querySelector('.title')
-const resetBtn=document.querySelector('.reset')
-const correctSound =  document.getElementById('correct');
-const wrongSound = document.getElementById('wrong')
-const colors = ['#f08080', '#90ee90', '#20b2aa', '#ffb6c1', '#87cefa'];
+const boxes = document.querySelectorAll(".box");
+const dragableBoxContainer = document.querySelector(".dragable-box-container");
+const scoreContainer = document.querySelector(".score");
+const title = document.querySelector(".title");
+const resetBtn = document.querySelector(".reset");
+const correctSound = document.getElementById("correct");
+const wrongSound = document.getElementById("wrong");
+const colors = ["#f08080", "#90ee90", "#20b2aa", "#ffb6c1", "#87cefa"];
 let numbers = [];
-let newBox = document.createElement('div');
+let newBox = document.createElement("div");
 let score = 0;
 
 for (i = 0; i < boxes.length; i++) {
-    numbers.push(i);
+  numbers.push(i);
 }
 let random = shuffle(numbers);
 
-window.addEventListener('DOMContentLoaded', startGame)
+window.addEventListener("DOMContentLoaded", startGame);
 
-newBox.addEventListener('dragstart', dragStart)
-newBox.addEventListener('dragend', dragEnd)
+newBox.addEventListener("dragstart", dragStart);
+newBox.addEventListener("dragend", dragEnd);
 
-resetBtn.addEventListener('click',()=>{
-    if(confirm("Press 'OK' if you want to reset the score ?")) {
-    score=0;
-    scoreContainer.textContent=0
-    localStorage.removeItem('score')
-    }
-})
+resetBtn.addEventListener("click", () => {
+  if (confirm("Press 'OK' if you want to reset the score ?")) {
+    score = 0;
+    scoreContainer.textContent = 0;
+    localStorage.removeItem("score");
+  }
+});
 
 for (box of boxes) {
-    box.addEventListener('dragover', dragOver)
-    box.addEventListener('dragenter', dragEnter)
-    box.addEventListener('dragleave', dragLeave)
-    box.addEventListener('drop', dragDrop)
+  box.addEventListener("dragover", dragOver);
+  box.addEventListener("dragenter", dragEnter);
+  box.addEventListener("dragleave", dragLeave);
+  box.addEventListener("drop", dragDrop);
 }
 
 function dragStart() {
-    this.className += ' hold'
-    setTimeout(() => this.className += ' invisible', 0)
+  this.className += " hold";
+  setTimeout(() => (this.className += " invisible"), 0);
 }
 
 function dragEnd() {
-    this.className = 'dragable-box'
+  this.className = "dragable-box";
 }
 
 function dragOver(e) {
-    e.preventDefault()
+  e.preventDefault();
 }
 
 function dragEnter(e) {
-    e.preventDefault()
-    this.className += ' hovered'
+  e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-    this.className = 'box'
+  this.className = "box";
 }
 
 function dragDrop() {
-    this.className = 'box'
-    this.append(newBox)
-    this.style.borderWidth = 0;
-    random = shuffle(numbers);
-    let currentBox = this.style.borderColor
-    updateScore(currentBox)
-    setTimeout(() => startGame(), 500)
+  this.className = "box";
+  this.append(newBox);
+  this.style.borderWidth = 0;
+  random = shuffle(numbers);
+  let currentBox = this.style.borderColor;
+  updateScore(currentBox);
+  setTimeout(() => startGame(), 500);
 }
-
-
 
 function startGame() {
+  newBox.className = "dragable-box";
+  newBox.setAttribute("draggable", true);
+  dragableBoxContainer.append(newBox);
+  let randomBg = Math.floor(Math.random() * colors.length);
+  newBox.style.backgroundColor = colors[randomBg];
+  title.style.color = colors[randomBg];
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].style.border = `5px solid ${colors[numbers[i]]}`;
+    //    boxes[i].style.boxShadow=`0px 0px 2px 5px ${colors[numbers[i]]}`
+  }
+}
 
-    newBox.className = 'dragable-box';
-    newBox.setAttribute('draggable', true)
-    dragableBoxContainer.append(newBox)
-    let randomBg = Math.floor(Math.random() * colors.length)
-    newBox.style.backgroundColor = colors[randomBg];
-    title.style.color = colors[randomBg]
-    for (i = 0; i < boxes.length; i++) {
-        boxes[i].style.border = `5px solid ${colors[numbers[i]]}`
-        //    boxes[i].style.boxShadow=`0px 0px 2px 5px ${colors[numbers[i]]}`
-    }
-}    
-
-score=localStorage.getItem('score')
-scoreContainer.textContent=score;
-
+score = localStorage.getItem("score");
+scoreContainer.textContent = score;
 
 function shuffle(o) {
-    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+}
 
 function updateScore(currentBox) {
-    let userBox = newBox.style.backgroundColor;
-    if (currentBox == userBox) {
-        score++;
-        scoreContainer.textContent = score;
-        localStorage.setItem('score', score);
-        correctSound.currentTime = 0;
-        correctSound.play()
-    } else {
-        score--;
-        scoreContainer.textContent = score;
-        localStorage.setItem('score', score);
-        wrongSound.currentTime = 0;
-        wrongSound.play()
-    }
+  let userBox = newBox.style.backgroundColor;
+  if (currentBox == userBox) {
+    score++;
+    scoreContainer.textContent = score;
+    localStorage.setItem("score", score);
+    correctSound.currentTime = 0;
+    correctSound.play();
+  } else {
+    score--;
+    scoreContainer.textContent = score;
+    localStorage.setItem("score", score);
+    wrongSound.currentTime = 0;
+    wrongSound.play();
+  }
 }
-console.log(random)
+
+// CHECK WHETHER DEVICE IS MOBILE
+function checkMobile() {
+  if (window.innerWidth < 768) {
+    document.querySelector(".home--mobile").classList.add("active");
+  } else {
+    document.querySelector(".home--mobile").classList.remove("active");
+  }
+}
+document.addEventListener("DOMContentLoaded", checkMobile);
+window.addEventListener("resize", checkMobile);
